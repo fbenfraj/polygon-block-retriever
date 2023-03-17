8386336f8cd2bf0f4430cc844dd7bf42d821768e
fbenfraj <farouk.ben-fraj@outlook.com>
2023-03-17 14:09:21 +0100

    Add ws package dependency and setup WebSocket logic for newHeads

--- src/web3/web3.service.ts
+++ src/web3/web3.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ethers } from 'ethers';
+import { WebSocket } from 'ws';
 
 @Injectable()
 export class Web3Service {
   private readonly alchemyApiKey: string;
-  private readonly alchemyRpcUrl: string;
+  public readonly webSocket: WebSocket;
   public provider: ethers.JsonRpcProvider;
 
   constructor(private configService: ConfigService) {
     this.alchemyApiKey = this.configService.get<string>('ALCHEMY_API_KEY');
-    this.alchemyRpcUrl = `https://polygon-mainnet.g.alchemy.com/v2/${this.alchemyApiKey}`;
+    this.provider = new ethers.JsonRpcProvider(
+      `https://polygon-mainnet.g.alchemy.com/v2/${this.alchemyApiKey}`,
+    );
+    this.webSocket = new WebSocket(
+      `wss://polygon-mainnet.g.alchemy.com/v2/${this.alchemyApiKey}`,
+    )
+      .on('open', () => {
+        this.subscribeToNewHeads();
+      })
+      .on('message', (data) => {
+        console.log('data: ', data);
+      })
+      .on('error', (error) => {
+        console.log('error: ', error);
+      })
+      .on('close', (code, reason) => {
+        console.log('close: ', code, reason);
+      });
   }
 
-  async onModuleInit(): Promise<void> {
-    this.provider = new ethers.JsonRpcProvider(this.alchemyRpcUrl);
-    try {
-      const blockNumber = await this.provider.getBlockNumber();
-      console.log('Connected to Polygon Mainnet');
-      console.log('Current block number:', blockNumber);
-    } catch (error) {
-      console.error('Error connecting to Polygon Mainnet:', error);
-    }
+  private subscribeToNewHeads() {
+    const payload = {
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'eth_subscribe',
+      params: ['newHeads'],
+    };
+
+    this.webSocket.send(JSON.stringify(payload));
   }
 }
